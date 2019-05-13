e819c9e27882d953370e419aba923d91c300a0b2
Aaron Iker <aaron.iker@ottonova.de>
2019-05-13 11:53:42 +0200

    dirty menu, submenu active fix

--- themes/next/shared/partials/navigation/menu.php
+++ themes/next/shared/partials/navigation/menu.php
@@ -15,13 +15,36 @@ $icons = [
     'hosting_plans' => 'briefcase'
 ];
 
+$url = strtok($_SERVER['REQUEST_URI'], '?');
+
 echo '<ul>';
 foreach(clone iMSCP_Registry::get('navigation') as $obj) {
     if($obj->_visible) {
 
-        $url = strtok($_SERVER['REQUEST_URI'], '?');
+        $submenu = '';
+        $parentActive = false;
 
-        echo $obj->_active || $obj->_uri == $url || count(array_filter($obj->_pages, function($r) {
+        if(count($obj->_pages)) {
+            ob_start();
+            echo '<span></span><ul>';
+            foreach($obj->_pages as $sub) {
+                if($sub->_visible) {
+                    if($sub->_active || $sub->_uri == $url) {
+                        $parentActive = true;
+                        echo '<li class="active">';
+                    } else {
+                        echo '<li>';
+                    }
+                    echo '<a href="'.$sub->_uri.'">'.$sub->_label.'</a>';
+                    echo '</li>';
+                }
+            }
+            echo '</ul>';
+            $submenu = ob_get_contents();
+            ob_end_clean();
+        }
+
+        echo $parentActive || $obj->_active || $obj->_uri == $url || count(array_filter($obj->_pages, function($r) {
             return $r->_active || $r->_uri == $url;
         })) ? '<li class="active opened">' : '<li>';
 
@@ -29,19 +52,7 @@ foreach(clone iMSCP_Registry::get('navigation') as $obj) {
         echo isset($icons[$obj->_class]) ? '<i data-eva="'.$icons[$obj->_class].'" data-eva-fill="currentColor"></i>' : '<i data-eva="arrow-right-outline" data-eva-fill="currentColor"></i>';
         echo $obj->_label;
         echo '</a>';
-
-        if(count($obj->_pages)) {
-            echo '<span></span><ul>';
-            foreach($obj->_pages as $sub) {
-                if($sub->_visible) {
-                    echo $sub->_active || $sub->_uri == $url ? '<li class="active">' : '<li>';
-                    echo '<a href="'.$sub->_uri.'">'.$sub->_label.'</a>';
-                    echo '</li>';
-                }
-            }
-            echo '</ul>';
-        }
-
+        echo $submenu;
         echo '</li>';
     }
 }
